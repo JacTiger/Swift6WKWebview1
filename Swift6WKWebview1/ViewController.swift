901c795d0470d977df1cc4e9c448e9ade6d5d3fd
藤岡正哉 <mac12140321@icloud.com>
2021-02-17 19:58:19 +0900

    Initial Commit

---
+++ Swift6WKWebview1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6WKWebview1
+//
+//  Created by 藤岡正哉 on 2021/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
